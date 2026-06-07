af03932cd5e5adaa01cd536d78f4930905bb0ca6
Pravin <pravinky7294@gmail.com>
2026-06-08 01:30:48 +0530

    SecurityConfing improve

--- backend/src/main/java/com/smartjobportal/config/SecurityConfig.java
+++ backend/src/main/java/com/smartjobportal/config/SecurityConfig.java
@@ -75,6 +75,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+//        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
